efe92615032aa8f339219fa2be17934b74a8e75c
Carlos García <deivicarlos@gmail.com>
2018-04-04 18:47:11 -0400

    Updates

--- src/app.js
+++ src/app.js
@@ -3,51 +3,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from the expense dashboard page
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from the add expense page
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from the edit expense page
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from the help page
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404!
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('App'));
+ReactDOM.render(<AppRouter />, document.getElementById('App'));
