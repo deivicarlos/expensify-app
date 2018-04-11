c92fbf4ba28ae13eeca3b8a9baf345091fc8d7a0
Carlos García <deivicarlos@gmail.com>
2018-04-11 19:56:26 -0400

    Updates

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 
 //import './utils.js';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { setTextFilter } from './actions/filters';
+import { addExpense } from './actions/expenses';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({
+    description: 'Water bill',
+    amount: 15000
+}));
+
+store.dispatch(addExpense({
+    description: 'Gas bill',
+    amount: 10000
+}));
+
+store.dispatch(setTextFilter('water'));
+
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+
+//console.log(showExpenses(store.expenses, store.filters));
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('App'));
